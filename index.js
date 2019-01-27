3d921a3b5426a63fdb8ce197fd2d0af7374a916d
Harzz <basuharish97@gmail.com>
2019-01-27 16:04:44 +0530

    added deps for express and serialization for passport

--- index.js
+++ index.js
@@ -3,38 +3,64 @@ var DatabaseHelper = require("./core/DatabaseHelper")
 var express = require('express');
 var app = express();
 var router = express.Router()
-var path = __dirname+"/views/";
+var path = __dirname + "/views/";
 var bodyParser = require("body-parser")
+var flash = require("connect-flash")
+var passport = require("passport")
+var session = require("express-session")
+var sessionSecret = {
+  secret: 'apsc',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: true
+  }
+}
 
-
-app.use(bodyParser.urlencoded({extended:false}))
+require("./core/passport")
+require("./api/models/User")
+//to accept form values
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
+
+app.use(session(sessionSecret))
+//connect-flash
+app.use(flash())
+
+//to use passport and session
+app.use(passport.initialize())
+app.use(passport.session())
+//to use static files
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
-app.use(express.static(__dirname+"/public/"));
-app.set('view engine','pug')
-app.set('views','./views')
+app.use(express.static(__dirname + "/public/"));
+//set the template engine
+app.set('view engine', 'pug')
+app.set('views', './views')
 
-router.use('/api',require("./api/routes"))
+router.use('/api', require("./api/routes"))
 
-router.use((req,res,next)=>{
-    console.log("/"+req.method);
-    console.log("Body",req.body)
-    next();
+
+router.use((req, res, next) => {
+  console.log("/" + req.method);
+  console.log("Body", req.body)
+  next();
 });
 
-router.get("/",(req,res) =>{
-    res.render("index");
-  });
- 
-router.get("/dashboard",(req,res)=>{
-    res.render("dashboard/index")
-  })
-app.use("/",router);
+router.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.use("*",function(req,res){
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard/index")
+})
+app.use("/", router);
+
+app.use("*", function (req, res) {
   res.render("404");
 });
 
-app.listen(80,function(){
+app.listen(80, function () {
   console.log("Live at Port 3000");
 });
