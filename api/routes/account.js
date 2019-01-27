85ea8633f26287e9900ac66467ad29578659281e
Harzz <basuharish97@gmail.com>
2019-01-27 16:03:22 +0530

    routes for login

--- api/routes/account.js
+++ api/routes/account.js
@@ -1,8 +1,15 @@
 var express = require('express')
 var controller = require('../controllers/UserController')
 var router = new express.Router()
+var passport = require("passport")
+, LocalStrategy = require('passport-local').Strategy
 
-router.post('/login', controller.registerUser)
+
+router.post('/login', passport.authenticate('login',{
+    successRedirect:'/dashboard',
+    failureRedirect:"/404",
+    failureFlash:true,
+  }))
 router.post('/register',controller.registerUser)
 
 module.exports = router
